c2a5304dc7df158e03c352cf652d1472babd620b
MrCylops <mrcylops@gmail.com>
2018-07-26 15:26:56 +0300

    Upload

---
+++ Main.vb
@@ -0,0 +1,60 @@
+﻿Module Main
+    Dim steampath = ""
+    Dim p() As Process
+    Sub Main()
+        Console.Title = "Cylops's Old Steam Friend Interface Launcher"
+        Console.ForegroundColor = ConsoleColor.DarkYellow
+        Console.WriteLine("Cylops's Old Steam Friend Interface Launcher")
+        Console.WriteLine("---------------------------------------------")
+        System.Threading.Thread.Sleep(1000)
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("Auf der Suche nach Steams Standort...")
+        System.Threading.Thread.Sleep(1500)
+        Dim readValue = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", Nothing)
+        If My.Computer.FileSystem.DirectoryExists(readValue) Then
+            If My.Computer.FileSystem.FileExists(readValue & "/Steam.exe") Then
+                steampath = readValue
+                Console.WriteLine("Steam hat es gefunden =>")
+                Console.ForegroundColor = ConsoleColor.Red
+                Console.Write(readValue + "!")
+            Else
+                MsgBox("Steam wurde woanders hinbewegt. Bitte ziehe zurück zu seinem alten Standort! 7781", "Error!")
+                End
+            End If
+        Else
+            MsgBox("Kann den Standort von Steam nicht lesen! Als Administrator ausführen! 93834", "Error!")
+            End
+        End If
+        Console.ForegroundColor = ConsoleColor.DarkYellow
+        Console.WriteLine(" ")
+        Console.WriteLine("---------------------------------------------")
+        p = Process.GetProcessesByName("Steam")
+        If p.Count > 0 Then
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine("Steam ist arbeit. Wir werden jetzt für dich schließen ...")
+            Try
+                Process.GetProcessesByName("Steam")(0).Kill()
+            Catch ex As Exception
+                MsgBox("Ein Fehler ist aufgetreten. Fehler ist >>" + ex.ToString, "Error!")
+            End Try
+            Console.WriteLine(" ")
+            Console.ForegroundColor = ConsoleColor.DarkYellow
+            Console.WriteLine("Initiieren Steam [w/ Old Friends UI]")
+            System.Threading.Thread.Sleep(2000)
+            Process.Start(readValue + "/Steam.exe", "-nofriendsui -nochatui")
+            Console.ForegroundColor = ConsoleColor.Green
+            Console.WriteLine("Steam begonnen mit Erfolg. Dieses Programm wird sich in 5 Sekunden schließen...")
+            System.Threading.Thread.Sleep(5000)
+            End
+        Else
+            Console.WriteLine("Initiieren Steam [w/ Old Friends UI]")
+            System.Threading.Thread.Sleep(2000)
+            Process.Start(readValue + "/Steam.exe", "-nofriendsui -nochatui")
+            Console.ForegroundColor = ConsoleColor.Green
+            Console.WriteLine("Steam begonnen mit Erfolg. Dieses Programm wird sich in 5 Sekunden schließen...")
+            System.Threading.Thread.Sleep(5000)
+            End
+        End If
+    End Sub
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("Old Steam Friends UI Launcher")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("")>
+<Assembly: AssemblyCopyright("Copyrighted By Cylops ©  2018 - 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("eaaf35b3-1497-4f84-86d9-290a015dbe02")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.1.3.3")>
+<Assembly: AssemblyFileVersion("0.1.3.3")>
